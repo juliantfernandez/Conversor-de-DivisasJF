27aad2327daae28f9f3df35c940460cac08e3da7
juliantfernandez <juliant.fernandez0611@gmail.com>
2021-07-12 14:09:18 -0300

    Finalizacion de proyecto final

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,260 @@
+//FUNCION DE EMULADOR//
+function convertir()
+{
+    var valor = parseFloat($("#cantidad").val());
+    var convertirDe = $("#convertirDe").val();
+    var convertirA = $("#convertirA").val();
+    var dolar = 95.35;
+    var dolarBlue = 175.00;
+    var real = 18.30;
+    var euro = 112.50;
+    var bitcoin = 3727229.26;
+    var ethereum = 230454.09;
+    resultado = 0;
+
+//PESO Y DOLAR
+//Peso a Dolar
+   if(convertirDe==1&&convertirA==2){
+       resultado=valor/dolar;
+   }
+ //dolar a peso
+   else if(convertirDe==2&&convertirA==1){
+    resultado=valor*dolar;
+}
+
+
+
+//PESO Y DOLAR BLUE
+//peso a Dolar Blue
+ else if(convertirDe==1&&convertirA==3){
+     resultado=valor/dolarBlue;
+ }
+//dolar Blue a peso
+ else if(convertirDe==3&&convertirA==1){
+     resultado=valor*dolarBlue;
+ }
+
+
+//PESO Y EURO
+   //peso a euro
+   else if(convertirDe==1&&convertirA==5){
+       resultado=valor/euro;
+   }
+
+  //euro a peso
+else if(convertirDe==5&&convertirA==1){
+    resultado=valor*euro;
+}
+
+
+
+//DOLAR Y EURO
+//dolar a euro
+else if(convertirDe==2&&convertirA==5){
+    resultado=valor*(dolar/euro);
+}
+
+//euro a dolar
+else if(convertirDe==5&&convertirA==2){
+    resultado=valor*(euro/dolar);
+}
+
+//DOLAR BLUE Y EURO
+//dolar Blue a euro
+else if(convertirDe==3&&convertirA==5){
+    resultado=valor*(dolarBlue/euro);
+}
+
+//euro a dolar Blue
+else if(convertirDe==5&&convertirA==3){
+    resultado=valor*(euro/dolarBlue);
+}
+
+//BITCOIN Y PESO
+//peso a btc
+else if(convertirDe==1&&convertirA==6){
+    resultado=valor/bitcoin;
+}
+
+//btc a peso
+else if(convertirDe==6&&convertirA==1){
+    resultado=valor*bitcoin;
+}
+
+
+
+//ETHEREUM Y PESO
+//peso a eth
+else if(convertirDe==1&&convertirA==7){
+    resultado=valor/ethereum;
+}
+
+//eth a peso 
+else if(convertirDe==7&&convertirA==1){
+    resultado=valor*ethereum;
+}
+
+
+
+//BITCOIN Y ETHEREUM
+//btc a ethereum
+else if(convertirDe==6&&convertirA==7){
+    resultado=valor*(bitcoin/ethereum);
+}
+
+//eth a btc
+else if(convertirDe==7&&convertirA==6){
+    resultado=valor*(ethereum/bitcoin);
+}
+
+
+
+//BTC Y DOLAR
+//btc a dolar
+else if(convertirDe==6&&convertirA==2){
+    resultado=valor*(bitcoin/dolar);
+}
+
+//dolar a btc
+else if(convertirDe==2&&convertirA==6){
+    resultado=valor*(dolar/bitcoin);
+}
+
+//BTC Y DOLAR BLUE
+//btc a Dolar Blue
+else if(convertirDe==6&&convertirA==3){
+    resultado=valor*(bitcoin/dolarBlue);
+}
+
+//dolarblue a btc
+else if(convertirDe==3&&convertirA==6){
+    resultado=valor*(dolarBlue/bitcoin);
+}
+
+
+//ETH Y DOLAR
+//eth a dolar
+else if(convertirDe==7&&convertirA==2){
+    resultado=valor*(ethereum/dolar);
+}
+
+//dolar a eth
+else if(convertirDe==2&&convertirA==7){
+    resultado=valor*(dolar/ethereum);
+}
+
+//ETH Y DOLAR BLUE
+//eth a dolarBlue
+else if(convertirDe==7&&convertirA==3){
+    resultado=valor*(ethereum/dolarBlue);
+}
+
+//dolarBlue a eth
+else if(convertirDe==3&&convertirA==7){
+    resultado=valor*(dolarBlue/ethereum);
+}
+
+
+//BTC Y EURO
+//btc a euro
+else if(convertirDe==6&&convertirA==5){
+    resultado=valor*(bitcoin/euro);
+}
+
+//euro a btc
+else if(convertirDe==5&&convertirA==6){
+    resultado=valor*(euro/bitcoin);
+}
+
+//ETH Y EURO
+//eth a euro
+else if(convertirDe==7&&convertirA==5){
+    resultado=valor*(ethereum/euro);
+}
+//euro a eth
+else if(convertirDe==5&&convertirA==7){
+    resultado=valor*(euro/ethereum);
+}
+
+
+
+//REAL Y PESO
+ //Peso a Real
+ else if(convertirDe==1&&convertirA==4){
+    resultado=valor/real;
+}
+//Real a Peso
+else if(convertirDe==4&&convertirA==1){
+    resultado= valor*real;
+}
+
+
+//REAL Y DOLAR
+//real a dolar
+else if(convertirDe==4&&convertirA==2){
+   resultado=valor/dolar;
+}
+
+//dolar a real
+else if(convertirDe==2&&convertirA==4){
+   resultado=valor*dolar;
+}
+
+//REAL Y DOLAR BLUE
+//real a dolar Blue
+else if(convertirDe==4&&convertirA==3){
+   resultado=valor/dolarBlue;
+}
+
+//dolarBlue a real
+else if(convertirDe==3&&convertirA==4){
+   resultado=valor*dolarBlue;
+}
+
+//REAL Y EURO
+//real a euro
+else if(convertirDe==4&&convertirA==5){
+   resultado=valor/euro;
+}
+
+//euro a real
+else if(convertirDe==5&&convertirA==4){
+resultado=valor*euro;
+}
+
+//REAL Y BITCOIN
+//real a btc
+else if(convertirDe==4&&convertirA==6){
+   resultado=valor/bitcoin;
+}
+
+//btc a real
+else if(convertirDe==6&&convertirA==4){
+   resultado=valor*bitcoin;
+}
+
+//REAL Y ETH
+//real a eth
+else if(convertirDe==4&&convertirA==7){
+   resultado=valor/ethereum;
+}
+
+//eth a real
+else if(convertirDe==7&&convertirA==4){
+   resultado=valor*ethereum;
+}
+
+
+
+
+//peso a peso, dolar a dolar, euro a euro, btc a btc y eth a eth;
+else{
+    resultado=valor;
+}
+$("#resultado").html("Resultado $" + resultado.toFixed(2));
+}
+
+//APIS
 $(document).ready(function() {
     //dolar Oficial
     const dolarOficialAPI = 'https://api-dolar-argentina.herokuapp.com/api/dolaroficial';
@@ -87,187 +344,34 @@ $(document).ready(function() {
     
 });
 
+//STORAGE Y JSON
 
-//FUNCION DE EMULADOR//
-function convertir()
-{
-    var valor = parseFloat($("#cantidad").val());
-    var convertirDe = $("#convertirDe").val();
-    var convertirA = $("#convertirA").val();
-    var dolar = 95.35;
-    var euro = 113.61;
-    var bitcoin = 3727229.26;
-    var ethereum = 230454.09;
-    resultado = 0;
+const divisasDisponibles = [{ divisa: "dolar", valor: 95.35},
+                            {divisa: "dolarblue", valor: 175.00},
+                            { divisa: "BTC", valor:3727229.26},
+                            {divisa: "euro", valor: 112.50},
+                            {divisa: "ETH", valor:230454.09},
+                            {divisa: "real", valor: 18.30}];
+console.log(divisasDisponibles);
 
+const guardarLocal = (clave, valor) => {localStorage.setItem(clave,valor)};
 
+guardarLocal("Divisas Disponibles", JSON.stringify(divisasDisponibles));
 
-    //PESO Y DOLAR
-   //Peso a Dolar
-   if(convertirDe==1&&convertirA==2){
-       resultado=valor/dolar;
-   }
-
- //dolar a peso
-   else if(convertirDe==2&&convertirA==1){
-    resultado=valor*dolar;
+class Divisas{
+    constructor(obj){
+        this.divisa = obj.divisa;
+        this.valor= obj.valor;
+    }
+    sumarIva(){
+        this.valor = this.valor*1.21;
+    }
 }
 
+const divisasAlmacenadas = JSON.parse(localStorage.getItem("Divisas Disponibles"));
+const misDivisas = [];
 
-
-//PESO Y EURO
-   //peso a euro
-   else if(convertirDe==1&&convertirA==3){
-       resultado=valor/euro;
-   }
-
-  //euro a peso
-else if(convertirDe==3&&convertirA==1){
-    resultado=valor*euro;
+for(const objeto of divisasAlmacenadas){
+    misDivisas.push(new Divisas(objeto));
 }
-
-
-
-//DOLAR Y EURO
-//dolar a euro
-else if(convertirDe==2&&convertirA==3){
-    resultado=valor*(dolar/euro);
-}
-
-//euro a dolar
-else if(convertirDe==3&&convertirA==2){
-    resultado=valor*(euro/dolar);
-}
-
-
-
-//BITCOIN Y PESO
-//peso a btc
-else if(convertirDe==1&&convertirA==4){
-    resultado=valor/bitcoin;
-}
-
-//btc a peso
-else if(convertirDe==4&&convertirA==1){
-    resultado=valor*bitcoin;
-}
-
-
-
-//ETHEREUM Y PESO
-//peso a eth
-else if(convertirDe==1&&convertirA==5){
-    resultado=valor/ethereum;
-}
-
-//eth a peso 
-else if(convertirDe==5&&convertirA==1){
-    resultado=valor*ethereum;
-}
-
-
-
-//BITCOIN Y ETHEREUM
-//btc a ethereum
-else if(convertirDe==4&&convertirA==5){
-    resultado=valor*(bitcoin/ethereum);
-}
-
-//eth a btc
-else if(convertirDe==5&&convertirA==4){
-    resultado=valor*(ethereum/bitcoin);
-}
-
-
-
-//BTC Y DOLAR
-//btc a dolar
-else if(convertirDe==4&&convertirA==2){
-    resultado=valor*(bitcoin/dolar);
-}
-
-//dolar a btc
-else if(convertirDe==2&&convertirA==4){
-    resultado=valor*(dolar/bitcoin);
-}
-
-
-//ETH Y DOLAR
-//eth a dolar
-else if(convertirDe==5&&convertirA==2){
-    resultado=valor*(ethereum/dolar);
-}
-
-//dolar a eth
-else if(convertirDe==2&&convertirA==5){
-    resultado=valor*(dolar/ethereum);
-}
-
-
-//BTC Y EURO
-//btc a euro
-else if(convertirDe==4&&convertirA==3){
-    resultado=valor*(bitcoin/euro);
-}
-
-//euro a btc
-else if(convertirDe==3&&convertirA==4){
-    resultado=valor*(euro/bitcoin);
-}
-
-//ETH Y EURO
-//eth a euro
-else if(convertirDe==5&&convertirA==3){
-    resultado=valor*(ethereum/euro);
-}
-//euro a eth
-else if(convertirDe==3&&convertirA==5){
-    resultado=valor*(euro/ethereum);
-}
-
-//peso a peso, dolar a dolar, euro a euro, btc a btc y eth a eth;
-else{
-    resultado=valor;
-}
-$("#resultado").html("Resultado $" + resultado.toFixed(2));
-}
-
-
-////FUNCION DE ICONOS CON JQUERY/////
-$("#seccionValores").prepend("<h5 style='text-align: center; margin-top: 45px;'>Click en los iconos para ver sus valores!</h5>");
-
-$("#muestraValorDolar").click(function(){
-    $("#valorDolar").fadeIn(1000)});
-
-$("#muestraValorEuro").click(function(){
-    $("#valorEuro").fadeIn(1000)});
-
-$("#muestraValorBTC").click(function(){
-    $("#valorBTC").fadeIn(1000)});
-
-$("#muestraValorETH").click(function(){
-    $("#valorETH").fadeIn(1000)
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(misDivisas);
